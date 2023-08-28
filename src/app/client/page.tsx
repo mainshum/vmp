b554a37afe5c3f379caf0704db3ea75c765b99bd
mainshum <doseit@gmail.com>
2023-08-28 21:54:17 +0200

    verify if it is a faulty link

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -4,7 +4,7 @@ import { getBaseUrl } from "@/lib/utils";
 import { headers } from "next/headers";
 
 async function PageServer() {
-  const res = await fetch(`${getBaseUrl()}/api/postings`, {
+  const res = await fetch(`https://vmp-sigma.vercel.app/api/postings`, {
     headers: headers(),
   });
 
