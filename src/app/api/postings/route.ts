68b01191198ef0a701ceaa36ce0c77666226d46f
mainshum <doseit@gmail.com>
2023-08-28 22:00:29 +0200

    remove header 'modification'

--- src/app/api/postings/route.ts
+++ src/app/api/postings/route.ts
@@ -2,13 +2,13 @@ import { MOCK_STATE_HEADER } from "@/lib/const";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  const hs = req.headers;
+  // const hs = req.headers;
 
-  if (hs.get(MOCK_STATE_HEADER) === "ERROR")
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 },
-    );
+  // if (hs.get(MOCK_STATE_HEADER) === "ERROR")
+  //   return NextResponse.json(
+  //     { error: "Internal Server Error" },
+  //     { status: 500 },
+  //   );
 
   return NextResponse.json(
     Array(10)
