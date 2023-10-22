30a68d38f4051472a582d8815ec3a8c65f596935
mainshum <doseit@gmail.com>
2023-10-22 17:58:59 +0200

    moved request form into a dialog (#10)

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -3,6 +3,17 @@ import { getVMPSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { ROUTES } from "@/lib/const";
 import { RequestsTable } from "./request-table";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { RequestForm } from "@/components/create-request";
+import { buttonVariants } from "@/components/ui/button";
 
 const getRequests = () =>
   db.request.findMany({
@@ -23,6 +34,19 @@ async function PageServer() {
 
   return (
     <section className="flex flex-col py-8">
+      <Dialog>
+        <DialogTrigger className={buttonVariants()}>
+          Create new request
+        </DialogTrigger>
+        <DialogContent className="max-h-full overflow-y-auto sm:max-h-[75%]">
+          <DialogHeader>
+            <DialogTitle className="pb-4">Job request</DialogTitle>
+            <DialogDescription>
+              <RequestForm />
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
       <RequestsTable requests={requests} />
     </section>
   );
