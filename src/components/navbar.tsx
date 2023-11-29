aad50dfbc9c6d1017be7147f804187705be376b8
mainshum <doseit@gmail.com>
2023-11-29 10:43:26 +0100

    VMP-83: restyle navbar (#18)
    * restyle navbar
    
    * fix sign-in props problem

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 import Icons from "./icons";
+import { UserCircle } from "lucide-react";
 
 function Navbar() {
   return (
-    <header className="fixed inset-x-0 top-0 z-[10] flex h-14 justify-center border-b border-zinc-300 bg-stone-100 py-2">
-      <nav className="container flex items-center justify-between">
-        <Link href="/" className="flex items-center gap-2">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
-          <p className="hidden md:block">VMP</p>
+    <header className="fixed inset-x-0 top-0 z-[10] flex justify-center border-b bg-black py-2">
+      <nav className="flex w-full items-center justify-between px-6">
+        <Link href="/" className="flex items-center gap-3">
+          <Icons.logo />
+          <p className="hidden text-2xl font-bold tracking-widest text-white md:block">
+            VMP
+          </p>
         </Link>
+        <UserCircle className="h-8 w-8 text-white" />
       </nav>
     </header>
   );
