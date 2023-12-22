202e717ee44dd58a308c5242d98b07a4982fc336
mainshum <doseit@gmail.com>
2023-12-22 11:17:18 +0100

    simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import { getVMPSession } from "@/lib/auth";
-import { ROUTES } from "@/lib/const";
-import { redirect } from "next/navigation";
-import { match } from "ts-pattern";
-
 export default async function Home() {
-  const session = await getVMPSession();
-
-  if (!session) return redirect(ROUTES.SIGIN);
-
   return (
-    match(session.user.role)
-      // in the future we're likely to have some kind of home page,
-      // for the time being go to postings
-      .with("CLIENT", () => redirect(ROUTES.CUSTOMER.REQUESTS.LIST))
-      .with("NONE", () => redirect(ROUTES.ROLE_SELECT))
-      .otherwise((r) => {
-        throw Error(`${r} not implemented`);
-      })
+    <>
+      <section className="p-32">
+        <h1 className="text-3xl font-bold">
+          IT procurement platform driven by technology
+        </h1>
+        <p className="pt-4 text-xl">
+          submit IT job requests to receive selection of offers from our{" "}
+          <b>trusted vendors</b> <br />
+          gain market insight and select the best <br />
+          use AI to help you choose
+        </p>
+      </section>
+      <div className="fixed bottom-0 flex aspect-square w-[300%] translate-x-[-32.5%] translate-y-[95%] justify-center rounded-full bg-black pt-32">
+        <p className="basis-[50vw] text-center text-2xl text-white">
+          post anonymized IT job requests and receive a wide selection of offers
+          from our <b>trusted vendors</b>
+        </p>
+      </div>
+    </>
   );
 }
