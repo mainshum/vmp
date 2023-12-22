23b5261304e1177f8c89ad12b3185de852565794
mainshum <doseit@gmail.com>
2023-12-22 10:14:37 +0100

    fix: use relative link

--- src/components/trpc-provider.tsx
+++ src/components/trpc-provider.tsx
@@ -12,7 +12,7 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = (p) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: "/api/trpc",
         }),
       ],
       transformer: superjson,
