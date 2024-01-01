e3af387460b77ef4374aeefaa99c90ac81ba8b2d
mainshum <doseit@gmail.com>
2024-01-01 16:35:29 +0100

    fix test

--- cypress/e2e/offers.cy.ts
+++ cypress/e2e/offers.cy.ts
@@ -3,5 +3,5 @@ import {} from "nanoid";
 
 it("rendering: request not found", () => {
   cy.intercept("GET");
-  cy.visit(ROUTES.VENDOR.OFFERS.CREATE("6bbdb131-3e2a-48cb-b43e-066bfac8d10c"));
+  cy.visit(ROUTES.OFFERS.CREATE("6bbdb131-3e2a-48cb-b43e-066bfac8d10c"));
 });
